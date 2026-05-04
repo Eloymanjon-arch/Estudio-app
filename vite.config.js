ac600ea283a28ee0391a7be17bd613fc724accb8
Eloy manjon jimenez <eloymanjonjimenez@MacBook-Air-de-Eloy.local>
2026-05-04 13:35:33 +0200

    fix deploy base path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/Estudio-app/"   // 👈 NOMBRE EXACTO DE TU REPO
 });
