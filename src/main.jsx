c6102041a772677ac8ef491522274caa5cd26078
Eloy manjon jimenez <eloymanjonjimenez@MacBook-Air-de-Eloy.local>
2026-05-04 13:05:51 +0200

    fix manifest pwa

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,10 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>
 )
+
+// 🔥 PWA FIX (SIN "/")
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
+    navigator.serviceWorker.register('sw.js')
+  })
 }
